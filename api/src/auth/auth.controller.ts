0bd64bdf11f4e44949f1e6fec5250ef9fe99c2b8
Jo du Plessis <jo@joduplessis.com>
2022-03-06 15:38:40 +0100

    Added more schemas

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -5,69 +5,130 @@ import { UsersService } from '../users/users.service'
 import { AuthService } from './auth.service'
 
 export default async (fastify, opts) => {
-    fastify.post('/auth/login', {}, async (request, reply) => {
-        try {
-            const { email, password } = request.body
-            const user: User = await AuthService.validateUser(fastify, email, password)
+    fastify.post(
+        '/auth/login',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['email', 'password'],
+                    properties: {
+                        email: { type: 'string' },
+                        password: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const { email, password } = request.body
+                const user: User = await AuthService.validateUser(fastify, email, password)
 
-            if (!user) throw Error('not found')
+                if (!user) throw Error('not found')
 
-            const userId = user.id
-            const claims = await AuthService.getClaims(fastify, userId)
+                const userId = user.id
+                const claims = await AuthService.getClaims(fastify, userId)
 
-            // The claims object above will always have the most up to date claims
-            // So safe for us to clear their cache
-            await AuthService.clearInvalidations(fastify, userId)
+                // The claims object above will always have the most up to date claims
+                // So safe for us to clear their cache
+                await AuthService.clearInvalidations(fastify, userId)
 
-            // Return the token containing the claims
-            const token = AuthService.getToken(fastify, userId, email, claims)
+                // Return the token containing the claims
+                const token = AuthService.getToken(fastify, userId, email, claims)
 
-            return { token }
-        } catch (error) {
-            throw Error(error)
+                return { token }
+            } catch (error) {
+                throw Error(error)
+            }
         }
-    })
+    )
 
-    fastify.post('/auth/signup', {}, async (request, reply) => {
-        try {
-            const { email, name, password } = request.body
-            let user: User = await UsersService.findOneWithEmail(fastify, email)
-            if (user) throw Error('User exists')
-            user = await UsersService.create(fastify, { email, name, password })
-            MailService.sendWelcome(fastify, email)
-            return user
-        } catch (error) {
-            throw Error(error)
+    fastify.post(
+        '/auth/signup',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['name', 'email', 'password'],
+                    properties: {
+                        name: { type: 'string' },
+                        email: { type: 'string' },
+                        password: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const { email, name, password } = request.body
+                let user: User = await UsersService.findOneWithEmail(fastify, email)
+                if (user) throw Error('User exists')
+                user = await UsersService.create(fastify, { email, name, password })
+                MailService.sendWelcome(fastify, email)
+                return user
+            } catch (error) {
+                throw Error(error)
+            }
         }
-    })
+    )
 
-    fastify.put('/auth/reset-password', {}, async (request, reply) => {
-        try {
-            const { email } = request.body
-            const token = uuidv4()
-            const user: any = await UsersService.findOneWithEmail(fastify, email)
+    fastify.put(
+        '/auth/reset-password',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['email'],
+                    properties: {
+                        email: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const { email } = request.body
+                const token = uuidv4()
+                const user: any = await UsersService.findOneWithEmail(fastify, email)
 
-            if (!user) throw 'Not found'
+                if (!user) throw 'Not found'
 
-            await UsersService.update(fastify, { id: user.id, resetToken: token })
-            await MailService.sendPasswordReset(fastify, email, token)
+                await UsersService.update(fastify, { id: user.id, resetToken: token })
+                await MailService.sendPasswordReset(fastify, email, token)
 
-            return { success: true, token }
-        } catch (error) {
-            throw Error(error)
+                return { success: true, token }
+            } catch (error) {
+                throw Error(error)
+            }
         }
-    })
+    )
 
-    fastify.put('/auth/update-password', {}, async (request, reply) => {
-        try {
-            const { email, resetToken, password } = request.body
-            const user: User = await UsersService.findOneWithEmailAndResetToken(fastify, email, resetToken)
+    fastify.put(
+        '/auth/update-password',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['resetToken', 'email', 'password'],
+                    properties: {
+                        resetToken: { type: 'string' },
+                        email: { type: 'string' },
+                        password: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const { email, resetToken, password } = request.body
+                const user: User = await UsersService.findOneWithEmailAndResetToken(fastify, email, resetToken)
 
-            if (!user) throw 'Not found'
+                if (!user) throw 'Not found'
 
-            return await UsersService.update(fastify, { id: user.id, password, resetToken: null })
-        } catch (error) {
-            throw Error(error)
+                return await UsersService.update(fastify, { id: user.id, password, resetToken: null })
+            } catch (error) {
+                throw Error(error)
+            }
         }
-    })
+    )
 }
--- api/src/files/files.controller.ts
+++ api/src/files/files.controller.ts
@@ -5,66 +5,151 @@ import { getKey } from '../util'
 import { S3 } from '../constants'
 
 export default async (fastify, opts) => {
-    const options = { preValidation: [fastify.authenticate] }
+    const options = {}
 
-    fastify.delete('/files/delete', options, async (request, reply) => {
-        try {
-            const file: string = request.query.filename
-            const key: string = getKey(file)
-            await FilesService.delete(fastify, key)
+    fastify.delete(
+        '/files/delete',
+        {
+            preValidation: [fastify.authenticate],
+            schema: {
+                querystring: {
+                    type: 'object',
+                    required: ['filename'],
+                    properties: {
+                        filename: {
+                            type: 'string',
+                        },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const file: string = request.query.filename
+                const key: string = getKey(file)
 
-            return { success: true }
-        } catch (e) {
-            return null
+                await FilesService.delete(fastify, key)
+
+                return { success: true }
+            } catch (e) {
+                return null
+            }
         }
-    })
+    )
 
-    fastify.post('/files/delete-all', options, async (request, reply) => {
-        try {
-            const { filenames } = request.body
-            const keys: string[] = filenames.map((file: string) => getKey(file))
-            await FilesService.deleteAll(fastify, keys)
+    fastify.post(
+        '/files/delete-all',
+        {
+            preValidation: [fastify.authenticate],
+            schema: {
+                body: {
+                    type: 'object',
+                    required: ['filenames'],
+                    properties: {
+                        filenames: {
+                            type: 'array',
+                            default: [],
+                        },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const { filenames } = request.body
+                const keys: string[] = filenames.map((file: string) => getKey(file))
 
-            return { success: true }
-        } catch (e) {
-            return null
+                await FilesService.deleteAll(fastify, keys)
+
+                return { success: true }
+            } catch (e) {
+                return null
+            }
         }
-    })
+    )
 
-    fastify.post('/files/download', options, async (request, reply) => {
-        try {
-            if (!request.body.token) throw Error('No token')
-            const url = FilesService.getSignedUrl(fastify, request.query.file)
-            reply.redirect(url)
-        } catch (e) {
-            return null
+    fastify.post(
+        '/files/download',
+        {
+            preValidation: [fastify.authenticate],
+            schema: {
+                querystring: {
+                    type: 'object',
+                    required: ['file'],
+                    properties: {
+                        file: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const url = await FilesService.getSignedUrl(fastify, request.query.file)
+
+                reply.redirect(url)
+            } catch (e) {
+                return null
+            }
         }
-    })
+    )
 
-    fastify.get('/files/get-signed-url', options, async (request, reply) => {
-        try {
-            const url = FilesService.getSignedUrl(fastify, request.query.file)
+    fastify.get(
+        '/files/get-signed-url',
+        {
+            preValidation: [fastify.authenticate],
+            schema: {
+                querystring: {
+                    type: 'object',
+                    required: ['file'],
+                    properties: {
+                        file: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const url = await FilesService.getSignedUrl(fastify, request.query.file)
 
-            return { url }
-        } catch (e) {
-            return null
+                return { url }
+            } catch (e) {
+                return null
+            }
         }
-    })
+    )
 
-    fastify.post('/files/get-upload-url', options, async (request, reply) => {
-        try {
-            const Expires = S3.EXPIRES
-            const Bucket = process.env.AWS_S3_BUCKET
-            const { folder, filename, mime, secure } = request.body as any
-            const Key = folder + '/' + uuidv4() + '.' + filename
-            const ACL = secure ? 'private' : 'public-read'
-            const ContentType = mime
-            const params = { Bucket, Key, Expires, ACL, ContentType }
-            const signedUrl = fastify.s3.getSignedUrl('putObject', params)
+    fastify.post(
+        '/files/get-upload-url',
+        {
+            schema: {
+                preValidation: [fastify.authenticate],
+                body: {
+                    type: 'object',
+                    required: ['folder', ' filename', 'mime', 'secure'],
+                    properties: {
+                        folder: { type: 'string' },
+                        filename: { type: 'string' },
+                        mime: { type: 'string' },
+                        secure: { type: 'boolean' },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            try {
+                const Expires = S3.EXPIRES
+                const Bucket = process.env.AWS_S3_BUCKET
+                const { folder, filename, mime, secure } = request.body
+                const Key = folder + '/' + uuidv4() + '.' + filename
+                const ACL = secure ? 'private' : 'public-read'
+                const ContentType = mime
+                const params = { Bucket, Key, Expires, ACL, ContentType }
+                const signedUrl = fastify.s3.getSignedUrl('putObject', params)
 
-            return { signedUrl }
-        } catch (error) {
-            reply.code(error.statusCode).send(error)
+                return { signedUrl }
+            } catch (error) {
+                reply.code(error.statusCode).send(error)
+            }
         }
-    })
+    )
 }
